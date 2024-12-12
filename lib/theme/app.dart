b84fa45e4fa8323a1f3b43b9959f0472571dfc94
StarPandaBeg <kshmurakov@mail.ru>
2024-12-13 01:53:30 +0300

    feat: change app theme

--- lib/theme/app.dart
+++ lib/theme/app.dart
@@ -2,6 +2,23 @@ import 'package:flutter/material.dart';
 
 ThemeData appTheme() {
   return ThemeData(
-    scaffoldBackgroundColor: const Color(0xff1e1e1e),
+    scaffoldBackgroundColor: const Color(0xFF1c1a1a),
+    fontFamily: "Montserrat",
+    textTheme: const TextTheme(
+      headlineLarge: TextStyle(
+        color: Colors.white,
+        fontSize: 28,
+        fontWeight: FontWeight.w600,
+      ),
+      headlineMedium: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+      ),
+      bodyMedium: TextStyle(
+        color: Color(0xff999999),
+        fontSize: 14,
+      ),
+    ),
   );
 }
