7e36172e501af65d93cb7c998c7ae79f35e611d3
StarPandaBeg <kshmurakov@mail.ru>
2024-12-17 23:14:37 +0300

    feat: update test screen

--- lib/screens/mqtt-test/screen.dart
+++ lib/screens/mqtt-test/screen.dart
@@ -1,10 +1,75 @@
+import 'package:cattyled_app/api/commands.dart';
+import 'package:cattyled_app/store/mqtt.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ScreenMqttTest extends StatelessWidget {
   const ScreenMqttTest({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center();
+    return Center(
+      child: Column(
+        children: [
+          BlocBuilder<MqttBloc, MqttState>(
+            builder: (context, state) =>
+                Text(state.isConnected ? "Connected" : "Disconnected"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final store = context.read<MqttBloc>();
+              store.add(MqttCommandEvent(CommandPower(state: true)));
+            },
+            child: const Text("Power ON"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final store = context.read<MqttBloc>();
+              store.add(MqttCommandEvent(CommandPower(state: false)));
+            },
+            child: const Text("Power OFF"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final store = context.read<MqttBloc>();
+              store.add(
+                MqttCommandEvent(CommandColor(color: const Color(0xffff0000))),
+              );
+            },
+            child: const Text("Set Color RED"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final store = context.read<MqttBloc>();
+              store.add(
+                MqttCommandEvent(CommandColor(color: const Color(0xff0000FF))),
+              );
+            },
+            child: const Text("Set Color BLUE"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final store = context.read<MqttBloc>();
+              store.add(MqttCommandEvent(CommandMode(mode: LampMode.classic)));
+            },
+            child: const Text("Set Mode Classic"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final store = context.read<MqttBloc>();
+              store.add(MqttCommandEvent(CommandMode(mode: LampMode.rainbow)));
+            },
+            child: const Text("Set Mode Rainbow"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final store = context.read<MqttBloc>();
+              store.add(MqttCommandEvent(CommandWink()));
+            },
+            child: const Text("Do Wink"),
+          ),
+        ],
+      ),
+    );
   }
 }
